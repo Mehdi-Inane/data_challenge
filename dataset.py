aae67ea2bfac3be21bdebafe4861ecf0b51be279
Mehdi-Inane <inaneahmedmehdi@gmail.com>
2024-03-17 13:33:30 +0100

    adding code for training

--- dataset.py
+++ dataset.py
@@ -1,4 +1,43 @@
-import torch
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
+from config import Config
+import os
+from PIL import Image
+
+config = Config()
+randomCrop = transforms.RandomCrop(config.input_size)
+centerCrop = transforms.CenterCrop(config.input_size)
+toTensor   = transforms.ToTensor()
+class UnlabeledDataset(Dataset):
+  """
+  Returns a dataset of images, and the get method returns the normalized image as a [1,512,512] tensor,
+  its transform in the same shape and the seed used for the transforms
+  """
+
+  def __init__(self,set,split,transform,image_dir,transform_crop = 0):
+    self.image_dir = image_dir
+    images = [os.path.join(image_dir, file) for file in os.listdir(image_dir) if file.endswith('.png')]
+    images = sorted(images, key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
+    unlabeled_idx = list(range(400,2000))
+    self.mode = set
+    self.transform_crop = transform_crop
+    split_stop = int((split) * len(unlabeled_idx))
+    if set == "val":
+      self.image_files = images[:split_stop]
+    if set == "train":
+      self.image_files = images[split_stop:]
+    self.transform = transform
+
+  def __getitem__(self,idx):
+    img = Image.open(self.image_files[idx]) # So far it's still grayscale
+    output = img.copy()
+    if self.mode == "train" and self.transform_crop > 0:
+        output = randomCrop(img)
+    #input = toTensor(centerCrop(input))
+    input = toTensor(img)
+    output = toTensor(output)
+    return input,output
+
+  def __len__(self):
+    return len(self.image_files)
 
---
+++ train.py
@@ -0,0 +1,86 @@
+from config import Config
+from dataset import UnlabeledDataset
+import torch
+from loss import *
+from model import WNet
+import matplotlib.pyplot as plt
+import pickle
+from utils import *
+
+config = Config()
+
+
+#### Building datasets and dataloaders
+
+img_path = '/content/data/train/images'
+train_dataset = UnlabeledDataset('train',0.2,None,img_path,1)
+val_dataset = UnlabeledDataset('val',0.2,None,img_path,1)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size,num_workers = 2, shuffle=True)
+val_dataloader   = torch.utils.data.DataLoader(val_dataset,   batch_size=4,num_workers = 2, shuffle=False)
+
+
+#### Model 
+autoencoder = WNet().to('cuda')
+ncutloss = NCutLoss2D()
+optimizerE = torch.optim.Adam(autoencoder.U_encoder.parameters(), lr=0.003)
+optimizerW = torch.optim.Adam(autoencoder.parameters(), lr=0.003)
+model_base_name = 'content/model_checkpoints/WNET_'
+
+###Training
+
+autoencoder.train()
+progress_images, progress_expected = next(iter(val_dataloader))
+for epoch in range(config.num_epochs):
+        running_loss = 0.0
+        ncutloss = []
+        reconloss = []
+        for i, [inputs, outputs] in enumerate(train_dataloader, 0):
+
+            if config.showdata:
+                print(inputs.shape)
+                print(outputs.shape)
+                print(inputs[0])
+                plt.imshow(inputs[0].permute(1, 2, 0))
+                plt.show()
+
+            if torch.cuda.is_available():
+                inputs  = inputs.cuda()
+                outputs = outputs.cuda()
+
+            optimizerE.zero_grad()
+            segmentations = autoencoder.forward_encoder(inputs)
+            l_soft_n_cut  = ncutloss(segmentations, inputs)
+            l_soft_n_cut.backward(retain_graph=False)
+            optimizerE.step()
+            ncutloss.append(l_soft_n_cut)
+
+            optimizerW.zero_grad()
+
+            segmentations, reconstructions = autoencoder.forward(inputs)
+
+            l_reconstruction = reconstruction_loss(
+                inputs if config.variationalTranslation == 0 else outputs,
+                reconstructions
+            )
+            reconloss.append(l_reconstruction)
+            l_reconstruction.backward(
+                retain_graph=False)  # We only need to do retain graph =true if we're backpropping from multiple heads
+            optimizerW.step()
+            if config.debug and (i%50) == 0:
+                print(i)
+            running_loss += l_reconstruction + l_soft_n_cut#loss.item()
+            if config.showSegmentationProgress and i == 0: # If first batch in epoch
+                save_progress_image(autoencoder, progress_images, epoch)
+
+        epoch_loss = running_loss / len(train_dataloader.dataset)
+        print(f"Epoch {epoch} loss: {epoch_loss:.6f}")
+
+        if config.saveModel:
+            torch.save(f'{model_base_name + str(epoch)}.pth')
+
+        with open('n_cut_loss.pkl','ab') as f:
+          pickle.dump(ncutloss, f)
+
+        with open('reconstruction_loss.pkl','ab') as fp:
+          pickle.dump(reconloss, fp)
+
